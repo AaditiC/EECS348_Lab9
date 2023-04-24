0e98f0d378f0ef4237bcf277559d3a6c9b7ab362
AaditiC <123911761+AaditiC@users.noreply.github.com>
2023-04-23 19:52:37 -0500

    Add files via upload

---
+++ Lab9Task1/javascriptfile.js
@@ -0,0 +1,30 @@
+function get_paragraph_color() {
+    var border_R = document.getElementById("border_R").value;
+    var border_G = document.getElementById("border_G").value;
+    var border_B = document.getElementById("border_B").value;
+    var border_width = document.getElementById("border_width").value;
+    var bg_R = document.getElementById("bg_R").value;
+    var bg_G = document.getElementById("bg_G").value;
+    var bg_B = document.getElementById("bg_B").value;
+
+    var tag = document.getElementById("paragraph");
+    tag.style.borderColor = `rgb(${border_R},${border_G},${border_B})`;
+    tag.style.borderWidth = border_width
+    tag.style.backgroundColor = `rgb(${bg_R},${bg_G},${bg_B})`;
+ }
+
+ function validate() {
+    var pass1 = document.getElementById("pass1").value;
+    var pass2 = document.getElementById("pass2").value;
+
+
+    if (pass1.length < 8) {
+        alert("The length of the first password needs to be longer.")
+    } else if (pass2.length < 8) {
+        alert("The length of the second password needs to be longer.")
+    } else if (pass1 != pass2) {
+        alert("Both passwords need to match.")
+    } else {
+        alert("Success! You have completed your passwords.")
+    }
+}
